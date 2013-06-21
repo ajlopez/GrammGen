19a015bebbb8c99e548e1a9f7b9bddfa640b96c5
ajlopez <webmaster@ajlopez.com>
2013-06-21 17:08:00 -0300

    ILexerProcessor

--- Src/GrammGen.Tests/LexerTests.cs
+++ Src/GrammGen.Tests/LexerTests.cs
@@ -1,9 +1,9 @@
 ﻿namespace GrammGen.Tests
 {
     using System;
-    using System.Text;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
@@ -20,6 +20,49 @@
             Assert.IsNotNull(result);
             Assert.AreEqual("a", result.Value);
             Assert.AreEqual("Alpha", result.Type);
+
+            Assert.IsNull(lexer.NextToken());
+        }
+
+        [TestMethod]
+        public void GetLetterAAndLetterB()
+        {
+            Lexer lexer = new Lexer("ab");
+            lexer.Define("Alpha", 'a');
+            lexer.Define("Beta", 'b');
+
+            var result = lexer.NextToken();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("a", result.Value);
+            Assert.AreEqual("Alpha", result.Type);
+
+            result = lexer.NextToken();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("b", result.Value);
+            Assert.AreEqual("Beta", result.Type);
+
+            Assert.IsNull(lexer.NextToken());
+        }
+
+        [TestMethod]
+        public void GetWordWithLettersAOrB()
+        {
+            Lexer lexer = new Lexer("ab");
+            lexer.Define("AlphaBeta", Lexer.Or('a', 'b'));
+
+            var result = lexer.NextToken();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("a", result.Value);
+            Assert.AreEqual("AlphaBeta", result.Type);
+
+            result = lexer.NextToken();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("b", result.Value);
+            Assert.AreEqual("AlphaBeta", result.Type);
 
             Assert.IsNull(lexer.NextToken());
         }
--- Src/GrammGen/Lexer.cs
+++ Src/GrammGen/Lexer.cs
@@ -9,8 +9,7 @@
     public class Lexer
     {
         private TextReader reader;
-        private char ch;
-        private string name;
+        private IDictionary<string, ILexerProcessor> processors = new Dictionary<string, ILexerProcessor>();
 
         public Lexer(string text)
             : this(new StringReader(text))
@@ -22,10 +21,24 @@
             this.reader = reader;
         }
 
+        public interface ILexerProcessor
+        {
+            string Process(char ch, Lexer lexer);
+        }
+
+        public static ILexerProcessor Or(params char[] chars)
+        {
+            return new OrProcessor(chars);
+        }
+
         public void Define(string name, char ch)
         {
-            this.name = name;
-            this.ch = ch;
+            this.Define(name, new CharacterProcessor(ch));
+        }
+
+        public void Define(string name, ILexerProcessor processor)
+        {
+            this.processors[name] = processor;
         }
 
         public Token NextToken()
@@ -35,8 +48,15 @@
             if (ich < 0)
                 return null;
 
-            if (ich == this.ch)
-                return new Token(this.name, this.ch.ToString());
+            char ch = (char)ich;
+
+            foreach (var name in this.processors.Keys)
+            {
+                string value = this.processors[name].Process(ch, this);
+
+                if (value != null)
+                    return new Token(name, value);
+            }
 
             return null;
         }
@@ -45,8 +65,9 @@
         {
             int ich;
 
-            for (ich = this.NextChar(); ich >= 0 && char.IsWhiteSpace((char) ich); ich = this.NextChar())
-                ;
+            for (ich = this.NextChar(); ich >= 0; ich = this.NextChar())
+                if (!char.IsWhiteSpace((char)ich))
+                    break;
 
             return ich;
         }
@@ -54,6 +75,42 @@
         private int NextChar()
         {
             return this.reader.Read();
+        }
+
+        private class CharacterProcessor : ILexerProcessor
+        {
+            private char character;
+
+            public CharacterProcessor(char character)
+            {
+                this.character = character;
+            }
+
+            public string Process(char ch, Lexer lexer)
+            {
+                if (this.character == ch)
+                    return ch.ToString();
+
+                return null;
+            }
+        }
+
+        private class OrProcessor : ILexerProcessor
+        {
+            private char[] chars;
+
+            public OrProcessor(char[] chars)
+            {
+                this.chars = chars;
+            }
+
+            public string Process(char ch, Lexer lexer)
+            {
+                if (this.chars.Contains(ch))
+                    return ch.ToString();
+
+                return null;
+            }
         }
     }
 }
