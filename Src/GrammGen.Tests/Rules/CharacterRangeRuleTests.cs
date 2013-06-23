7e5f50d40c7caafb9796157fd17981030dbd7555
ajlopez <webmaster@ajlopez.com>
2013-06-23 14:36:43 -0300

    Character Range Rule first test

---
+++ Src/GrammGen.Tests/Rules/CharacterRangeRuleTests.cs
@@ -0,0 +1,33 @@
+﻿namespace GrammGen.Tests.Rules
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using GrammGen.Rules;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CharacterRangeRuleTests
+    {
+        [TestMethod]
+        public void ProcessLowerCaseLetters()
+        {
+            CharacterRangeRule rule = new CharacterRangeRule('a', 'z');
+
+            var result = rule.Process("a");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("a", result.Value);
+
+            for (char ch = 'b'; ch <= 'z'; ch++)
+                Assert.IsNotNull(rule.Process(ch.ToString()));
+
+            for (char ch = 'A'; ch <= 'Z'; ch++)
+                Assert.IsNull(rule.Process(ch.ToString()));
+
+            for (char ch = '0'; ch <= '9'; ch++)
+                Assert.IsNull(rule.Process(ch.ToString()));
+        }
+    }
+}
---
+++ Src/GrammGen/Rules/CharacterRangeRule.cs
@@ -0,0 +1,29 @@
+﻿namespace GrammGen.Rules
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class CharacterRangeRule : Rule
+    {
+        private char from;
+        private char to;
+
+        public CharacterRangeRule(char from, char to)
+        {
+            this.from = from;
+            this.to = to;
+        }
+
+        public override Element Process(ISource source)
+        {
+            int ich = source.NextChar();
+
+            if (ich < 0 || (char)ich < this.from || (char)ich > this.to)
+                return null;
+
+            return new Element(null, ((char)ich).ToString());
+        }
+    }
+}
