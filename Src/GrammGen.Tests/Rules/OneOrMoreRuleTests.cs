c61783439751bceae5c92c897c838519adf55bb7
ajlopez <webmaster@ajlopez.com>
2013-06-23 15:04:03 -0300

    One or More Rule first test

---
+++ Src/GrammGen.Tests/Rules/OneOrMoreRuleTests.cs
@@ -0,0 +1,24 @@
+﻿namespace GrammGen.Tests.Rules
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using GrammGen.Rules;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class OneOrMoreRuleTests
+    {
+        [TestMethod]
+        public void GetLowerCaseLetters()
+        {
+            OneOrMoreRule rule = new OneOrMoreRule(new CharacterRangeRule('a', 'z'));
+
+            var result = rule.Process("abcABC");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("abc", result.Value);
+        }
+    }
+}
---
+++ Src/GrammGen/Rules/OneOrMoreRule.cs
@@ -0,0 +1,30 @@
+﻿namespace GrammGen.Rules
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class OneOrMoreRule : Rule
+    {
+        private Rule rule;
+
+        public OneOrMoreRule(Rule rule)
+        {
+            this.rule = rule;
+        }
+
+        public override Element Process(ISource source)
+        {
+            var result = this.rule.Process(source);
+
+            if (result == null)
+                return null;
+
+            for (var newresult = this.rule.Process(source); newresult != null; newresult = this.rule.Process(source))
+                result = new Element(null, (string)result.Value + (string)newresult.Value);
+
+            return result;
+        }
+    }
+}
