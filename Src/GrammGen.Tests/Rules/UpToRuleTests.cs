6a7df5388284d2d7bf2dcb044c104eaedb7972b7
ajlopez <webmaster@ajlopez.com>
2013-07-07 08:22:14 -0300

    UpToRule first tests

---
+++ Src/GrammGen.Tests/Rules/UpToRuleTests.cs
@@ -0,0 +1,88 @@
+﻿namespace GrammGen.Tests.Rules
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using GrammGen.Rules;
+
+    [TestClass]
+    public class UpToRuleTests
+    {
+        [TestMethod]
+        public void ProcessQuotedString()
+        {
+            CharacterRule rule1 = new CharacterRule('"');
+            UpToRule rule = new UpToRule(rule1, rule1);
+
+            var result = rule.Process("\"foo\"");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("foo", result.Value);
+        }
+
+        [TestMethod]
+        public void ProcessQuotedStringIncludingQuotes()
+        {
+            CharacterRule rule1 = new CharacterRule('"');
+            UpToRule rule = new UpToRule(rule1, rule1, true);
+
+            var result = rule.Process("\"foo\"");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("\"foo\"", result.Value);
+        }
+
+        [TestMethod]
+        public void ProcessEmptyString()
+        {
+            CharacterRule rule1 = new CharacterRule('"');
+            UpToRule rule = new UpToRule(rule1, rule1);
+
+            var result = rule.Process("\"\"");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(string.Empty, result.Value);
+        }
+
+        [TestMethod]
+        public void ProcessLineComment()
+        {
+            StringRule rule1 = new StringRule("//");
+            CharacterRule rule2 = new CharacterRule('\n');
+            UpToRule rule = new UpToRule(rule1, rule2);
+
+            var result = rule.Process("// a comment\n");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(" a comment", result.Value);
+        }
+
+        [TestMethod]
+        public void RejectInteger()
+        {
+            CharacterRule rule1 = new CharacterRule('"');
+            UpToRule rule = new UpToRule(rule1, rule1);
+
+            Assert.IsNull(rule.Process("123"));
+        }
+
+        [TestMethod]
+        public void RaiseIfUnclosedString()
+        {
+            CharacterRule rule1 = new CharacterRule('"');
+            UpToRule rule = new UpToRule(rule1, rule1);
+
+            try
+            {
+                rule.Process("\"foo");
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("unclosed term", ex.Message);
+            }
+        }
+    }
+}
---
+++ Src/GrammGen/Rules/UpToRule.cs
@@ -0,0 +1,54 @@
+﻿namespace GrammGen.Rules
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class UpToRule : Rule
+    {
+        private Rule left;
+        private Rule right;
+        private bool combine;
+
+        public UpToRule(Rule left, Rule right)
+            : this(left, right, false)
+        {
+        }
+
+        public UpToRule(Rule left, Rule right, bool combine)
+        {
+            this.left = left;
+            this.right = right;
+            this.combine = combine;
+        }
+
+        public override Element Process(IParser source)
+        {
+            var rleft = this.left.Process(source);
+
+            if (rleft == null)
+                return null;
+
+            var result = string.Empty;
+
+            var rright = this.right.Process(source);
+
+            while (rright == null)
+            {
+                int ich = source.NextChar();
+
+                if (ich < 0)
+                    throw new Exception("unclosed term");
+
+                result += (char)ich;
+                rright = this.right.Process(source);
+            }
+
+            if (this.combine)
+                return new Element(null, Rule.Combine(Rule.Combine(rleft.Value, result), rright.Value));
+
+            return new Element(null, result);
+        }
+    }
+}
