443001c2164f6168a00163acc9996c5aca99fdc4
ajlopez <webmaster@ajlopez.com>
2013-09-29 16:17:38 -0300

    Calculator parse integer

---
+++ Samples/Calculator/Calculator.Tests/ExpressionParserTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Calculator.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ExpressionParserTests
+    {
+        [TestMethod]
+        public void ParseInteger()
+        {
+            ExpressionParser parser = new ExpressionParser("123");
+
+            IExpression expression = parser.ParseExpression();
+
+            Assert.IsNotNull(expression);
+            Assert.IsInstanceOfType(expression, typeof(ConstantExpression));
+
+            ConstantExpression cexpression = (ConstantExpression)expression;
+
+            Assert.AreEqual(123, cexpression.Value);
+
+            Assert.IsNull(parser.ParseExpression());
+        }
+    }
+}
---
+++ Samples/Calculator/Calculator/ExpressionParser.cs
@@ -0,0 +1,33 @@
+﻿namespace Calculator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using GrammGen;
+
+    public class ExpressionParser
+    {
+        private static Rule[] rules = new Rule[] 
+        {
+            Rule.Get("0-9").OneOrMore().Generate("Expression", obj => new ConstantExpression(int.Parse((string)obj)))
+        };
+
+        private Parser parser;
+
+        public ExpressionParser(string text)
+        {
+            this.parser = new Parser(text, rules);
+        }
+
+        public IExpression ParseExpression()
+        {
+            var element = this.parser.Parse("Expression");
+
+            if (element == null)
+                return null;
+
+            return (IExpression)element.Value;
+        }
+    }
+}
