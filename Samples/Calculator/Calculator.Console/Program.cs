d2ec760da8eac35a290d4a3f085463bc886d50d8
ajlopez <webmaster@ajlopez.com>
2013-09-29 17:34:19 -0300

    Calculator in console

---
+++ Samples/Calculator/Calculator.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Calculator.Console
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator");
+
+            while (true)
+            {
+                Console.Write("> ");
+                string line = Console.ReadLine();
+
+                line = line.Trim().ToLowerInvariant();
+
+                if (line == "exit")
+                    return;
+
+                ExpressionParser parser = new ExpressionParser(line);
+
+                for (IExpression expression = parser.ParseExpression(); expression != null; expression = parser.ParseExpression())
+                    Console.WriteLine(expression.Evaluate());
+            }
+        }
+    }
+}
